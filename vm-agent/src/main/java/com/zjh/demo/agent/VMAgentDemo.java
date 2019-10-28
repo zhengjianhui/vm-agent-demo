977b7d3326f0d89fbd117785cefa14c2a7382067
zhengjianhui <zhengjianhui@yunzhangfang.com>
2019-10-28 09:39:23 +0800

    update

--- vm-agent/src/main/java/com/zjh/demo/agent/VMAgentDemo.java
+++ vm-agent/src/main/java/com/zjh/demo/agent/VMAgentDemo.java
@@ -27,6 +27,26 @@ public class VMAgentDemo {
 
     }
 
+    /**
+     * 如果不存在 premain(String agentArgs, Instrumentation inst)
+     * 则会执行 premain(String agentArgs)
+     */
+    public static void premain(String agentArgs) {
+        System.out.println("=========premain方法执行2========");
+        System.out.println(agentArgs);
+    }
+
+
+    /**
+     * vm 启动后增加代理, 调用的是这个方法
+     * jattach pid load /absolute/path/to/agent/libagent.so true
+     */
+    public static void agentmain(String args, Instrumentation ins) {
+        System.out.println("=========agentmain========");
+        test();
+    }
+
+
     private static void test() {
         Thread t = new Thread(() -> {
             while (true) {
@@ -54,25 +74,6 @@ public class VMAgentDemo {
 
 
         t.start();
-    }
-
-    /**
-     * 如果不存在 premain(String agentArgs, Instrumentation inst)
-     * 则会执行 premain(String agentArgs)
-     */
-    public static void premain(String agentArgs) {
-        System.out.println("=========premain方法执行2========");
-        System.out.println(agentArgs);
-    }
-
-
-    /**
-     * vm 启动后增加代理, 调用的是这个方法
-     * jattach pid load /absolute/path/to/agent/libagent.so true
-     */
-    public static void agentmain(String args, Instrumentation ins) {
-        System.out.println("=========agentmain========");
-        test();
     }
 
 
